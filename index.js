ad7bb3e434a413087f4bca824dce6c5a6b4dc992
atripes <at@geolad.com>
2018-05-30 18:44:27 +0200

    provides the input_pw as arg (instead of postArg)

--- index.js
+++ index.js
@@ -792,7 +792,7 @@ class PdfTk {
      * @see {@link https://www.pdflabs.com/docs/pdftk-man-page/#dest-input-pw}
      */
     inputPw(password) {
-        this.postArgs.push(
+        this.args.push(
             'input_pw',
             password
         );
